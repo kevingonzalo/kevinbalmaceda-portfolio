34f32104e818f20ee65c8d95041d3a8c08e076ef
Kevin Gonzalo Balmaceda <91499415+kevingonzalo@users.noreply.github.com>
2022-06-04 11:04:59 -0300

    Add files via upload

---
+++ proyectos/WEB De Noticias/codigo.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.new-cards', {delay:500});
+  ScrollReveal().reveal('.cards-banner-two', {delay:500});
+  ScrollReveal().reveal('.cards-banner-one', {delay:500});
+  ScrollReveal().reveal('.social', {delay:500});
+  ScrollReveal().reveal('.footer', {delay:500});
+ 
